53dc1c827139d8b54c25bafdf255d9e31a0e6c45
oafnghh <minhhoanglee11@gmail.com>
2025-10-13 22:14:31 +0700

    feat: setup constructer onboarding

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets {
+  // ---------------------PATHS --------------------//
+  static const String logoPath = 'assets/images/logos';
+
+  //----------------------LOGO ---------------------//
+  static const String logo = '$logoPath/onboard-logo.png';
+  static const String logoGG ='$logoPath/gg-logo.png';
+}
